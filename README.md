8a3102435fc6f12efacd30402b5ea0100c8e9e90
Paweł Kotiuk <kotiuk@zohomail.eu>
2023-11-29 17:12:52 +0100

    Create base for the Spade project
    Co-authored-by: Arkadiusz Nowacki <androjus@users.noreply.github.com>
    Co-authored-by: Michał Wiszenko <mwiszenko@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Rozproszony system pomiarowy
 
 Repozytorium stworzone w ramach realizacji przedmiotu AASD Aktorowe i Agentowe systemy decyzyjne.
+
+### Uruchamianie
+
+Cały system można uruchomić za pomocą dockera
+
+```bash
+docker-compose up
+```
---
+++ app/app.py
@@ -0,0 +1,15 @@
+import os
+import spade
+
+class DummyAgent(spade.agent.Agent):
+    async def setup(self):
+        print("Hello World! I'm agent {}".format(str(self.jid)))
+
+async def main():
+    dummy = DummyAgent(
+        f'your_jid@{os.environ.get("XMPP_SERVER")}', os.environ.get("XMPP_PASSWORD")
+    )
+    await dummy.start()
+
+if __name__ == "__main__":
+    spade.run(main())
