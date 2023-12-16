81bd5b4e0cb88789b8a26b7620869fac22f22b92
Paweł Kotiuk <kotiuk@zohomail.eu>
2023-12-16 19:49:20 +0100

    Create simple monitor checking temperature

---
+++ app/monitor_terminal.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+import json
+import os
+import time
+
+import spade
+from spade.agent import Agent
+from spade.behaviour import PeriodicBehaviour
+
+class MonitorAgent(Agent):
+    '''periodacially sends query messages to all sensors'''
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    async def setup(self):
+        print(f"Monitor agent with id: {self.jid} initialized")
+        b = self.SendQuery(period=5)
+        self.add_behaviour(b)
+    
+    class SendQuery(PeriodicBehaviour):
+        async def run(self):
+            print(f"Monitor agent with id: {self.agent.jid} querying sensors")
+            msg = spade.message.Message(to="sensor@localhost")
+            msg.set_metadata("performative", "query")
+            await self.send(msg)
+
+            #receive all responses
+            responses = []
+            responses_timeout = 2
+            end_time = time.time() + responses_timeout
+            while True:
+                response = await self.receive(timeout=end_time - time.time())
+                if response:
+                    responses.append(response)
+                else:
+                    break
+                        
+            print(f"Monitor agent with id: {self.agent.jid} received {len(responses)}")
+            print(f"responses: {[resp.body for resp in responses]}")
+
+async def main():
+    monitor = MonitorAgent(
+        f'monitor@{os.environ.get("XMPP_SERVER")}',
+        os.environ.get("XMPP_PASSWORD"),
+    )
+    await monitor.start(auto_register=True)
+    await spade.wait_until_finished(monitor)
+
+if __name__ == "__main__":
+    spade.run(main())
