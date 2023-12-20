a04e20d39e247621d0581d428dafe8eb32fdba62
kowusek <kowusek@gmail.com>
2023-12-20 21:01:47 +0100

    working effector

--- app/effector.py
+++ app/effector.py
@@ -4,8 +4,57 @@ import os
 
 import spade
 from spade.agent import Agent
-from spade.behaviour import State
+from spade.behaviour import State, FSMBehaviour
 
+STATE_ONE = "STATE_ONE"
+STATE_TWO = "STATE_TWO"
+
+class StateOne(State):
+
+    def perform_internal_action(self):
+        pass
+
+    async def run(self):
+        print("Effector agent at state one")
+        self.perform_internal_action()
+        self.set_next_state(STATE_ONE)
+        msg = await self.receive(timeout=60)
+        if msg:
+            print(f"Effector agent received request: {msg}")
+            if msg.body == STATE_TWO:
+                self.set_next_state(STATE_TWO)
+                reply = msg.make_reply()
+                reply.set_metadata("performative", "agree")
+                reply.body = self.next_state
+            else:
+                reply = msg.make_reply()
+                reply.set_metadata("performative", "refuse")
+                reply.body = self.next_state
+            await self.send(reply)
+            
+
+class StateTwo(State):
+
+    def perform_internal_action(self):
+        pass
+
+    async def run(self):
+        print("Effector agent at state two")
+        self.perform_internal_action()
+        self.set_next_state(STATE_TWO)
+        msg = await self.receive(timeout=60)
+        if msg:
+            print(f"Effector agent received request: {msg}")
+            if msg.body == STATE_ONE:
+                self.set_next_state(STATE_ONE)
+                reply = msg.make_reply()
+                reply.set_metadata("performative", "agree")
+                reply.body = self.next_state
+            else:
+                reply = msg.make_reply()
+                reply.set_metadata("performative", "refuse")
+                reply.body = self.next_state
+            await self.send(reply)
 
 class EffectorAgent(Agent):
     def __init__(self, *args, **kwargs):
@@ -14,38 +63,23 @@ class EffectorAgent(Agent):
     async def setup(self):
         print(f"Effector agent with id: {self.jid} initialized")
         b = self.FlipFlop()
+        b.add_state(name=STATE_ONE, state=StateOne(), initial=True)
+        b.add_state(name=STATE_TWO, state=StateTwo())
+        b.add_transition(source=STATE_ONE, dest=STATE_TWO)
+        b.add_transition(source=STATE_ONE, dest=STATE_ONE)
+        b.add_transition(source=STATE_TWO, dest=STATE_ONE)
+        b.add_transition(source=STATE_TWO, dest=STATE_TWO)
         template = spade.template.Template()
         template.set_metadata("performative", "request")
         self.add_behaviour(b, template)
 
-    class FlipFlop(State):
-        """responds to request with agree/cancel and state in msg.body"""
-
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-
-        def perform_internal_action(self, state):
-            pass
-
-        def change_state(self, state):
-            self.set_next_state(state)
-            self.perform_internal_action(state)
-            print(f"Effector agent changed state to: {self.next_state}")  
-
-        async def run(self):
-            msg = await self.receive()
-            if msg:
-                print(f"Effector agent received request: {msg}")
-                if msg.body != self.next_state:
-                    self.change_state(msg.body)
-                    reply = msg.make_reply()
-                    reply.set_metadata("performative", "agree")
-                    reply.body = self.next_state
-                else:
-                    reply = msg.make_reply()
-                    reply.set_metadata("performative", "cancel")
-                    reply.body = self.next_state
-                await self.send(reply)
+    class FlipFlop(FSMBehaviour):
+        async def on_start(self):
+            print(f"FlipFlop starting at initial state {self.current_state}")
+    
+        async def on_end(self):
+            print(f"FSM finished at state {self.current_state}")
+            await self.agent.stop()
 
 async def main():
     effector = EffectorAgent(
@@ -54,8 +88,6 @@ async def main():
     )
     await effector.start(auto_register=True)
     await spade.wait_until_finished(effector)
-
-
 
 if __name__ == "__main__":
     spade.run(main())
